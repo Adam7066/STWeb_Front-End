fafe0c0ea16189e4f0b49a512b248da831205ff7
Smallten <smallten@smallten.me>
2023-03-08 23:55:24 +0800

    feat: init unplugin-vue-components and unplugin-auto-import

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 import WindiCSS from "vite-plugin-windicss"
 import { resolve } from "path"
+import Components from "unplugin-vue-components/vite"
+import { ElementPlusResolver } from "unplugin-vue-components/resolvers"
+import AutoImport from "unplugin-auto-import/vite"
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -10,5 +13,14 @@ export default defineConfig({
 			"@": resolve(__dirname, "./src"),
 		},
 	},
-	plugins: [vue(), WindiCSS()],
+	plugins: [
+		vue(),
+		WindiCSS(),
+		Components({
+			resolvers: [ElementPlusResolver()],
+		}),
+		AutoImport({
+			imports: ["vue", "vue-router"],
+		}),
+	],
 })
