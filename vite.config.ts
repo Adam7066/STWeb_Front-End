235b72307dbb8820180ea103ffb7c6cefc386186
Smallten <smallten@smallten.me>
2023-03-08 23:58:38 +0800

    fix: eslint and unplugin-auto-import problem

--- vite.config.ts
+++ vite.config.ts
@@ -20,7 +20,18 @@ export default defineConfig({
 			resolvers: [ElementPlusResolver()],
 		}),
 		AutoImport({
+			include: [
+				/\.[tj]sx?$/, // .ts, .tsx, .js, .jsx
+				/\.vue$/,
+				/\.vue\?vue/, // .vue
+				/\.md$/, // .md
+			],
 			imports: ["vue", "vue-router"],
+			eslintrc: {
+				enabled: false, // Default `false`
+				filepath: "./.eslintrc-auto-import.json", // Default `./.eslintrc-auto-import.json`
+				globalsPropValue: true, // Default `true`, (true | false | 'readonly' | 'readable' | 'writable' | 'writeable')
+			},
 		}),
 	],
 })
