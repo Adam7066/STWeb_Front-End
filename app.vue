b33a3d5c4af6506d5af0625691eed3d29013a4d0
Smallten <smallten@smallten.me>
2023-08-07 14:07:17 +0800

    feat: add title, favicon, home page

--- app.vue
+++ app.vue
@@ -3,3 +3,11 @@
     <NuxtPage />
   </NuxtLayout>
 </template>
+
+<script setup lang="ts">
+useHead({
+  title: '小十的家',
+  meta: [{ name: 'description', content: '小十的家' }],
+  link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,5 +1,11 @@
 <template>
-  <div></div>
+  <div>
+    <div
+      class="mx-auto mt-24 flex h-80 max-w-3xl items-center justify-center rounded-lg border shadow-2xl"
+    >
+      <div class="text-5xl font-bold">歡迎來到小十的家</div>
+    </div>
+  </div>
 </template>
 
 <script setup lang="ts"></script>
