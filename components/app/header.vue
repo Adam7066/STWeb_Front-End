fc773331bca4fbfa5bce623b351adbcf6b4e6614
Smallten <smallten@smallten.me>
2023-08-24 02:34:33 +0800

    feat: init tools page

--- components/app/header.vue
+++ components/app/header.vue
@@ -4,15 +4,9 @@
       <template #logo>
         <img class="ml-8 h-12 w-12" src="@/assets/images/logo.png" alt="logo" />
       </template>
-      <t-menu-item value="home" class="text-2xl">
-        <NuxtLink to="/">首頁</NuxtLink>
-      </t-menu-item>
-      <t-menu-item value="about" class="text-2xl">
-        <NuxtLink to="/about">關於我</NuxtLink>
-      </t-menu-item>
-      <t-menu-item value="tools" class="text-2xl">
-        <NuxtLink to="/tools">小工具</NuxtLink>
-      </t-menu-item>
+      <t-menu-item value="home" class="text-2xl" to="/">首頁</t-menu-item>
+      <t-menu-item value="about" class="text-2xl" to="/about">關於我</t-menu-item>
+      <t-menu-item value="tools" class="text-2xl" to="/tools">小工具</t-menu-item>
 
       <template #operations>
         <t-space class="mr-8" :size="25">
@@ -27,5 +21,3 @@
     </t-head-menu>
   </div>
 </template>
-
-<script setup lang="ts"></script>
