bc6909920ae4b9e995ef6a6c2841ddb635265826
Smallten <smallten@smallten.me>
2025-03-14 09:18:58 +0800

    feat: add RWD to login page

--- pages/login.vue
+++ pages/login.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex w-full flex-col items-center justify-center">
-    <div class="my-14 flex w-1/2 max-w-[450px] flex-col items-center justify-center gap-4">
-      <div class="mb-6 text-2xl font-semibold">歡迎登入 小十的家</div>
+    <div class="mx-4 my-8 flex max-w-[450px] flex-col items-center justify-center gap-4 lg:my-14 lg:w-1/2">
+      <div class="text-2xl font-semibold lg:mb-6">歡迎登入 小十的家</div>
 
       <div class="w-full rounded-md border border-gray-200 bg-white p-6">
         <t-form
